0f1f1660019bbc7d8d7a95fe901805652d96a45d
CaLicS <trans77903@naver.com>
2019-07-29 08:02:00 +0900

    chapter1

---
+++ CHAPTER1.py
@@ -0,0 +1,81 @@
+#chapter 1
+##연습문제 1
+##print("환영합니다.")
+##print("파이썬의 세계에 오신 것을 환영합니다.")
+##print("파이썬은 강력합니다.")
+
+##연습문제 2
+##print("반갑습니다. 파이썬!")
+##print(2*3/10)
+##print("Hello", "World", "!!!")
+
+##연습문제 3
+##print(24*7)
+
+##연습문제 4
+##import turtle
+##t=turtle.Turtle()
+##t.shape("turtle")
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+##t.rt(90)
+##t.fd(100)
+##t.rt(90)
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+
+##연습문제 5
+##import turtle
+##t=turtle.Turtle()
+##t.shape("turtle")
+##t.width(10)
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+
+##연습문제 6
+##import turtle
+##t=turtle.Turtle()
+##t.shape("turtle")
+##t.color("blue")
+##t.fd(100)
+
+##연습문제 7
+##import turtle
+##t=turtle.Turtle()
+##t.shape("square") #ex) arrow, turtle, circle, square, triangle
+##t.fd(100)
+
+##연습문제 8
+##import turtle
+##t=turtle.Turtle()
+##t.shape("turtle")
+##t.fd(100)
+##t.up()
+##t.goto(0, 100)
+##t.down()
+##t.fd(100)
+
+##연습문제 9
+##import turtle
+##t=turtle.Turtle()
+##t.shape("turtle")
+##t.circle(100)
+##t.up()
+##t.goto(200,0)
+##t.down()
+##t.circle(100)
+##t.up()
+##t.goto(-80, 120)
+##t.down()
+##t.circle(100)
+##t.up()
+##t.goto(100, 120)
+##t.down()
+##t.circle(100)
+##t.up()
+##t.goto(280, 120)
+##t.down()
+##t.circle(100)
