e49f12a34974041acd68342c8d1bf2ad7ffa5f91
CaLicS <trans77903@naver.com>
2019-07-29 08:04:53 +0900

    chapter3

---
+++ CHAPTER3.py
@@ -0,0 +1,52 @@
+#chapter 3
+##연습문제 1
+##x = int(input("x : "))
+##y = int(input("y : "))
+##print("두수의 합 : ", x + y)
+##print("두수의 차 : ", x - y)
+##print("두수의 곱 : ", x * y)
+##print("두수의 평균 : ", (x + y)/2)
+##print("큰수 : ", max(x,y))
+##print("작은수 : ", min(x,y))
+
+##연습문제 2
+##r = int(input("r : "))
+##h = int(input("h : "))
+##print("원기둥의 부피 : ", 3.141592*r**2*h)
+
+##연습문제 3
+##x = int(input("정수를 입력하시오 : "))
+##y = 0
+##
+##y += x % 10
+##x = x // 10
+##y = y + x % 10
+##x = x // 10
+##y = y + x % 10
+##x = x // 10
+##y = y + x % 10
+##
+##print("자리수의 합 : ", y)
+
+##연습문제 4
+##x1 = int(input("x1 : "))
+##y1 = int(input("y1 : "))
+##x2 = int(input("x2 : "))
+##y2 = int(input("y2 : "))
+##print("두 점 사이의 거리 : ", ((x1 - x2)**2 + (y1 - y2)**2)**0.5)
+
+##연습문제 7
+##import time
+##x = time.time()
+##hour = x // 60 // 60 % 24
+##minute = x // 60 % 60
+##print("현재시간 (영국 그리니치 시각) : ", hour, "시", minute, "분")
+
+##연습문제 8
+##x = int(input("물체의 무게를 입력하시오(킬로그램) : "))
+##y = int(input("물체의 속도를 입력하시오(미터/초) : "))
+##print("물체는", 1/2*x*y**2, "(줄)의 에너지를 가지고 있다.")
+
+
+
+
