804b51bbf99397b7b28d4e70ed9c9523ecd9f900
CaLicS <trans77903@naver.com>
2019-08-26 12:52:17 +0900

    chapter6

---
+++ CHAPTER6.py
@@ -0,0 +1,35 @@
+##chapter 6
+##연습문제 1
+##for i in range(2, 101):
+##    if (i % 2 == 0):
+##        print(i, end=" ")
+
+##연습문제 2
+##year = 0
+##balance = 1000
+##
+##while balance < 2000:
+##    year = year + 1
+##    interest = balance * 0.07
+##    balance = balance + interest
+##print(year, "년 걸립니다.")
+
+##연습문제 3
+##n = 1234
+##sum = 0
+##while n > 0:
+##    digit = n % 10
+##    sum = sum + digit
+##    n = n // 10
+##print(sum)
+
+##연습문제 4
+import random
+
+a = random.randint(1,10)
+b = random.randint(1,10)
+com = a * b
+
+while True :
+    
+    usr = 
